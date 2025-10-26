c66fdcff689e940115d7618277ce9b4ec8bf26a7
Kacper Dygo <dygonkacper@gmail.com>
2025-10-26 16:41:37 +0100

    komit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { Wordle, WordleFieldState, type PlayerWordleField } from './wordleClass';
+
+const wordle = new Wordle(5);
+
+const exampleFieldData: PlayerWordleField[] = [
+  {
+    digit: "5",
+    state: WordleFieldState.Correct
+  },
+  {
+    digit: "3",
+    state: WordleFieldState.NotPresent
+  },
+  {
+    digit: "8",
+    state: WordleFieldState.NotPresent
+  },
+  {
+    digit: "7",
+    state: WordleFieldState.Misplaced
+  },
+  {
+    digit: "7",
+    state: WordleFieldState.NotPresent
+  }
+]
+
+onMounted(() => {
+  wordle.applyFieldData(exampleFieldData);
+
+  console.log(wordle.getAllData());
+})
+
+</script>
+
+
+
+<template>
+  
+</template>
+
+<style scoped></style>
